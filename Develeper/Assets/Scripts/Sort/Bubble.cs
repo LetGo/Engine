7416bbe91e3b69eed407b9e41feb3df5d77d4237
chengxue.zhao <369800228@qq.com>
2017-02-26 00:04:06 +0800

    排序算法学习

---
+++ Develeper/Assets/Scripts/Sort/Bubble.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubble : MonoBehaviour {
+
+    public int[] array;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("START"))
+        {
+            Sort();
+        }
+    }
+
+    void Sort(bool doStop = false)
+    {
+        if (array == null)
+        {
+            Debug.LogError("array is null");
+            return;
+        }
+        string msg = "";
+        for (int i = 0; i < array.Length; i++)
+        {
+            msg += array[i] + "  ";
+        }
+        Debug.Log(msg);
+        bool swap = false;
+
+        int lastUnsortIndex = array.Length - 1;
+        int index = 1;
+        do
+        {
+            swap = false;
+            for (int i = 0; i < lastUnsortIndex; i++)
+			{
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swap = true;
+                }
+			}
+
+            lastUnsortIndex-- ;
+
+
+            msg = "index : " + index + "--->";
+            index++;
+            for (int i = 0; i < array.Length; i++)
+            {
+                msg += array[i] + "  " ;
+            }
+            Debug.Log(msg);
+        } while (swap);
+
+    }
+
+}
